9bc362894f1005e07bf0d56c63f85c9b00b5f524
Paulina Krajnik <paulina.krajnik04@gmail.com>
2025-01-30 17:46:32 +0100

    added css and fixed page structure

--- src/app/page.js
+++ src/app/page.js
@@ -9,8 +9,10 @@ export default function Page() {
       <Header />
       <div>
         <h1>Guess the word!</h1>
-        <GameGrid />
-        <Chat />
+        <div className='main'>
+          <GameGrid className='game'/>
+          <Chat className='chat'/>
+        </div>
       </div>
     </div>
   );

f6ac00e6385224efa2c914b038fa066d2e47f232
Paulina Krajnik <paulina.krajnik04@gmail.com>
2025-02-01 15:33:52 +0100

    added some classNames

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import "./AccountPage.css"
 
 const API_URL = "http://localhost:3001";
 
@@ -31,13 +32,25 @@ const AccountPage = () => {
   if (!user) return <p>Loading user data...</p>;
 
   return (
-    <div>
-      <h1>Account Page</h1>
-      <p><strong>First Name:</strong> {user.firstname}</p>
-      <p><strong>Surname:</strong> {user.surname}</p>
-      <p><strong>Email:</strong> {user.email}</p>
-      <p><strong>Username:</strong> {user.username}</p>
-      <p><strong>Date of Birth:</strong> {new Date(user.dateofbirth).toISOString().split('T')[0]}</p>
+    <div className="account-container">
+      <h1 className="account-title">Account Page</h1>
+      <div className="account-details">
+        <div className="account-info">
+          <p><strong>First Name:</strong> {user.firstname}</p>
+          <p><strong>Surname:</strong> {user.surname}</p>
+          <p><strong>Email:</strong> {user.email}</p>
+          <p><strong>Username:</strong> {user.username}</p>
+          <p><strong>Date of Birth:</strong> {new Date(user.dateofbirth).toISOString().split('T')[0]}</p>
+        </div>
+      </div>
+      <div className="account-actions">
+        <a href="/edit-profile">
+          <button className="button edit-button">Edit Profile</button>
+        </a>
+        <button className="button back-button" onClick={() => window.history.back()}>
+          Back
+        </button>
+      </div>
     </div>
   );
 };
